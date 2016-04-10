eb45b927aa3f2ac4832f5aa8ad424a2fe461ba46
Liviu Anita <liviu.anita@bigstep.com>
2016-04-10 23:37:44 +0300

    Add pushForOcr

--- test.py
+++ test.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/do-ocr/')
+def pushForOCR():
+  strImagePath = request.args.get("path").lower()
+
+  ##Call OCR engine for image @ strImagePath here
+
 if __name__ == '__main__':
   app.debug = True
   app.run(host="0.0.0.0")
